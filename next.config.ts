8fb1cfce16377ddc176570965e77a8b09a916aee
rabotatony <rabotatony44@outlook.com>
2026-08-05 05:00:28 +0300

    fix(cf): webpack alias to exclude typescript from bundle

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  webpack: (config: any) => {
+    config.resolve = config.resolve || {};
+    config.resolve.alias = config.resolve.alias || {};
+    config.resolve.alias["typescript"] = false;
+    return config;
+  },
   reactStrictMode: false,
   typescript: {
     ignoreBuildErrors: true,
